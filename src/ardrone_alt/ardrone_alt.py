701404e17ee2f8bb37ae8fcafdd5463ea7416f6c
Christian Blum <christianb314@gmail.com>
2014-08-14 14:31:50 +0200

    increased to covariance to account for utm coordinates

--- src/ardrone_alt/ardrone_alt.py
+++ src/ardrone_alt/ardrone_alt.py
@@ -6,7 +6,7 @@ class ArdroneAlt():
   def __init__(self):
     # setup rospy and get parameters
     rospy.init_node("ardronealt")
-    self.covalt = rospy.get_param("~cov_alt", 0.1)
+    self.covalt = rospy.get_param("~cov_alt", 1.0)
     # setup main loop
     rospy.Subscriber("ardrone/navdata", Navdata, self.navdata_callback)
     self.pub = rospy.Publisher("odoalt", Odometry, queue_size=10)
@@ -25,8 +25,8 @@ class ArdroneAlt():
     msg.pose.pose.orientation.y = 0 
     msg.pose.pose.orientation.z = 0 
     msg.pose.pose.orientation.w = 0 
-    msg.pose.covariance = [99999, 0, 0, 0, 0, 0, 
-      0, 99999, 0, 0, 0, 0, 
+    msg.pose.covariance = [9999999, 0, 0, 0, 0, 0, 
+      0, 9999999, 0, 0, 0, 0, 
       0, 0, self.covalt, 0, 0, 0, 
       0, 0, 0, 99999, 0, 0, 
       0, 0, 0, 0, 99999, 0, 
